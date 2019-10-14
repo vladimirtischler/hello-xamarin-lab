2c38ff022ed5e7e45656584a689c06dc2f41f8f5
vladimirtischler <tischler.vladimir@gmail.com>
2019-10-14 08:18:39 +0200

    xamarin lab

---
+++ hello-xamarin-lab/hello-xamarin-lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_xamarin_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Xamarin Lab!");
+        }
+    }
+}
